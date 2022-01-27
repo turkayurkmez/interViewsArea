6c5927c93949efdaf30596ff9c95f4b2017d5349
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-01-28 00:36:38 +0300

    question 1

---
+++ questions/src/app/show-hide/show-hide.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-hide',
+  templateUrl: './show-hide.component.html',
+  styleUrls: ['./show-hide.component.css']
+})
+export class ShowHideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  isShowing :boolean = false;
+  clicked(){    
+    
+  }
+
+   message():string{
+    return "Hide";
+  }
+
+}
